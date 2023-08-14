e97cbf64aef4191d46229432d0ce78437ef5db1d
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 10:24:35 +0300

    fin

--- index.ts
+++ index.ts
@@ -4,7 +4,15 @@ import { map, catchError } from 'rxjs/operators';
 
 // Example 2: Observable that emits only the json key of the response object that is being returned from the request.
 
-const githubUsers = `https://api.github.com/users?per_page=2`;
+// const githubUsr = `https://api.github.com/users?per_page=2`;
+// const usr = ajax.getJSON(githubUsr);
+// const sub = usr.subscribe(
+//   (res) => console.log(res),
+//   (err) => console.error(err)
+// );
+
+// etim error - example 3
+const githubUsers = `https://api.github.com/error`;
 
 const users = ajax.getJSON(githubUsers);
 
